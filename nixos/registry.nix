893bbef255bf82720b59fa3b74ff20dfec48d84e
exruos <115374714+exruos@users.noreply.github.com>
2026-06-09 01:30:42 +0200

    update registry configuration for Zot container, adjusting volume path and adding tmpfiles rules

--- nixos/registry.nix
+++ nixos/registry.nix
@@ -4,10 +4,14 @@
   virtualisation.podman.enable = true;
   virtualisation.oci-containers.backend = "podman";
 
+  systemd.tmpfiles.rules = [
+    "d /var/lib/zot 0755 root root -"
+  ];
+
   virtualisation.oci-containers.containers.zot = {
     image = "ghcr.io/project-zot/zot-linux-amd64:v2.1.17";
     ports = [ "5000:5000" ];
-    volumes = [ "/srv/zot:/var/lib/registry" ];
+    volumes = [ "/var/lib/zot:/var/lib/registry" ];
   };
 
   networking.firewall.allowedTCPPorts = [ 5000 ];
